6abfa0f7fe3b83e04d98cab1eb2b4c8a6d76c4f5
Gred-git <90420916+Gred-git@users.noreply.github.com>
2024-07-08 17:23:13 -0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages";
+import Navbar from "./components/Navbar";
+ 
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+
+            </Routes>
+        </Router>
+    );
+}
+ 
+export default App;
